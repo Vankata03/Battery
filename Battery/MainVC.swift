958b8f5c7e9a325cd8b085c2dd010dbb10efdf5b
Ivan Terziev <ivanterziev38@gmail.com>
2020-08-12 16:29:04 +0300

    Initial commit - Done getting the battery and state, next will work on tracking changes and sending the level and state to the watch.

---
+++ Battery/MainVC.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  Battery
+//
+//  Created by Ivan Terziev on 12.08.20.
+//  Copyright © 2020 Ivan Terziev. All rights reserved.
+//
+
+import UIKit
+import WatchConnectivity
+
+class MainVC: UIViewController {
+
+    // MARK: - Properties
+    @IBOutlet var levelLabel: UILabel!
+    @IBOutlet var stateLabel: UILabel!
+    var level: Int = 0
+    var state: UIDevice.BatteryState = .unknown
+    var currentState: String = "unknown"
+    var device = UIDevice.current
+    
+    
+    // MARK: - App Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Initiate setup
+        setup()
+        
+    }
+    
+    
+    // MARK: - Methods
+    func setup() {
+        
+        // Enable battery monitoring
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        
+        // Get the current battery level
+        getLevel()
+        levelLabel.text = ("Your battery is at \n \(level)%")
+        
+        // Get the current battery state
+        getState()
+        stateLabel.text = ("Your battery is \n \(currentState)")
+        
+    }
+    
+    func getLevel() {
+        level = Int(device.batteryLevel * 100)
+    }
+    
+    func getState() {
+        
+        // Get the state
+        state = device.batteryState
+        
+        // Convert it into text
+        switch state {
+        case .full:
+            currentState = "full"
+        case .charging:
+            currentState = "charging"
+        case .unplugged:
+            currentState = "unplugged"
+        default:
+            currentState = "unknown"
+        }
+        
+    }
+    
+    
+    // MARK: - Objective-C Methods
+
+
+}
