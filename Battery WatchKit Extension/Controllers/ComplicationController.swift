aa4c88a4af797f1e6f23f2706c8c50485c0969f8
Ivan Terziev <ivanterziev38@gmail.com>
2020-08-15 01:23:49 +0300

    Got complications working, next step is to get live info

--- Battery WatchKit Extension/Controllers/ComplicationController.swift
+++ Battery WatchKit Extension/Controllers/ComplicationController.swift
@@ -8,7 +8,6 @@
 
 import ClockKit
 
-
 class ComplicationController: NSObject, CLKComplicationDataSource {
     
     // MARK: - Timeline Configuration
@@ -18,11 +17,14 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     }
     
     func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
+        let currentDate = Date()
+        handler(currentDate)
     }
     
     func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
+        var nextDate = Date()
+        nextDate.addTimeInterval(24 * 60 * 60)
+        handler(nextDate)
     }
     
     func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
@@ -32,8 +34,92 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        // Call the handler with the current timeline entry
-        handler(nil)
+        
+        // Info used
+        let gaugeProvider = CLKSimpleGaugeProvider.init(style: .fill, gaugeColor: UIColor.green, fillFraction: 0.7)
+        let textProvider = CLKSimpleTextProvider(text: "70")
+        let appText = CLKSimpleTextProvider(text: "Battery")
+        
+        // Go trough every supported compliction
+        switch complication.family {
+        case .utilitarianSmall:
+            
+            // Create the template
+            let template = CLKComplicationTemplateUtilitarianSmallRingText()
+            template.ringStyle = .closed
+            template.tintColor = UIColor.green
+            template.fillFraction = 0.7
+            template.textProvider = textProvider
+            
+            // Pass the current timeline entry
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+            handler(timelineEntry)
+            
+        case .circularSmall:
+            
+            // Create the template
+            let template = CLKComplicationTemplateCircularSmallRingText()
+            template.ringStyle = .closed
+            template.tintColor = UIColor.green
+            template.fillFraction = 0.7
+            template.textProvider = textProvider
+            
+            // Pass the current timeline entry
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+            handler(timelineEntry)
+            
+        case .extraLarge:
+            
+            // Create the template
+            let template = CLKComplicationTemplateExtraLargeRingText()
+            template.ringStyle = .closed
+            template.tintColor = UIColor.green
+            template.fillFraction = 0.7
+            template.textProvider = textProvider
+            
+            // Pass the current timeline entry
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+            handler(timelineEntry)
+            
+        case .graphicCorner:
+            
+            // Create the template
+            let template = CLKComplicationTemplateGraphicCornerGaugeText()
+            template.gaugeProvider = gaugeProvider
+            template.outerTextProvider = textProvider
+            
+            // Pass the current timeline entry
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+            handler(timelineEntry)
+            
+        case .graphicBezel:
+            
+            // Create the template
+            let template = CLKComplicationTemplateGraphicBezelCircularText()
+            let circularTemplate = CLKComplicationTemplateGraphicCircularClosedGaugeText()
+            circularTemplate.gaugeProvider = gaugeProvider
+            circularTemplate.centerTextProvider = textProvider
+            template.textProvider = appText
+            template.circularTemplate = circularTemplate
+            
+            // Pass the current timeline entry
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+            handler(timelineEntry)
+            
+        case .graphicCircular:
+            
+            // Create the template
+            let template = CLKComplicationTemplateGraphicCircularClosedGaugeText()
+            template.gaugeProvider = gaugeProvider
+            template.centerTextProvider = textProvider
+            
+            // Pass the current timeline entry
+            let timelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+            handler(timelineEntry)
+            
+        default:
+            handler(nil)
+        }
     }
     
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
@@ -46,11 +132,96 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         handler(nil)
     }
     
+    // MARK: - Request update
+    // Stopped because of the daily limit of 50 requests or so
+    //    func getNextRequestedUpdateDateWithHandler(handler: (NSDate?) -> Void) {
+    //        // Update every 15 minutes
+    //        handler(NSDate(timeIntervalSinceNow: 15 * 60))
+    //    }
+    
     // MARK: - Placeholder Templates
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        
+        // Info used
+        let gaugeProvider = CLKSimpleGaugeProvider.init(style: .fill, gaugeColor: UIColor.green, fillFraction: 0.7)
+        let textProvider = CLKSimpleTextProvider(text: "70")
+        let textProviderExtended = CLKSimpleTextProvider(text: "Your phone has 70% batter")
+        let appText = CLKSimpleTextProvider(text: "Battery")
+        
+        // Go trough every supported compliction
+        switch complication.family {
+        case .utilitarianSmall:
+            
+            // Create the template
+            let template = CLKComplicationTemplateUtilitarianSmallRingText()
+            template.ringStyle = .closed
+            template.tintColor = UIColor.green
+            template.fillFraction = 0.7
+            template.textProvider = textProvider
+            
+            // Pass the template
+            handler(template)
+            
+        case .circularSmall:
+            
+            // Create the template
+            let template = CLKComplicationTemplateCircularSmallRingText()
+            template.ringStyle = .closed
+            template.tintColor = UIColor.green
+            template.fillFraction = 0.7
+            template.textProvider = textProvider
+            
+            // Pass the template
+            handler(template)
+            
+        case .extraLarge:
+            
+            // Create the template
+            let template = CLKComplicationTemplateExtraLargeRingText()
+            template.ringStyle = .closed
+            template.tintColor = UIColor.green
+            template.fillFraction = 0.7
+            template.textProvider = textProviderExtended
+            
+            // Pass the template
+            handler(template)
+            
+        case .graphicCorner:
+            
+            // Create the template
+            let template = CLKComplicationTemplateGraphicCornerGaugeText()
+            template.gaugeProvider = gaugeProvider
+            template.outerTextProvider = textProvider
+            
+            // Pass the template
+            handler(template)
+            
+        case .graphicBezel:
+            
+            // Create the template
+            let template = CLKComplicationTemplateGraphicBezelCircularText()
+            let circularTemplate = CLKComplicationTemplateGraphicCircularClosedGaugeText()
+            circularTemplate.gaugeProvider = gaugeProvider
+            circularTemplate.centerTextProvider = textProvider
+            template.textProvider = appText
+            template.circularTemplate = circularTemplate
+            
+            // Pass the template
+            handler(template)
+            
+        case .graphicCircular:
+            
+            // Create the template
+            let template = CLKComplicationTemplateGraphicCircularClosedGaugeText()
+            template.gaugeProvider = gaugeProvider
+            template.centerTextProvider = textProvider
+            
+            // Pass the template
+            handler(template)
+            
+        default:
+            handler(nil)
+        }
     }
-    
 }
